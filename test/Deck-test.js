9300c6c8e032660c9a0dbb0da7c037d4e78fef5d
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-10 13:34:34 -0600

    Add instantiation test and implementation code

---
+++ test/Deck-test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+
+describe('Deck', () => {
+    it('Should be a function', () => {
+        const deck = new Deck();
+        expect(Deck).to.be.a('function');
+    });
+
+    it('should be an instance of deck', () => {
+        const deck = new Deck();
+        expect(deck).to.be.an.instanceof(Deck);
+      }); 
+});
