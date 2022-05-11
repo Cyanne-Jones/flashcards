d4cfeb320b9b566b38a9e4eca0cee8c32dc7bade
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-10 19:09:42 -0600

    Add test to assess Round has a turn property that begins at 0

--- test/Round-test.js
+++ test/Round-test.js
@@ -6,6 +6,13 @@ const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe ('Round', () => {
+
+    let card1;
+    let card2;
+    let card3;
+    let deck;
+    let round;
+
     it('Should be a function', () => {
         const round = new Round();
         expect(Round).to.be.a('function');
@@ -15,16 +22,22 @@ describe ('Round', () => {
         const round = new Round();
         expect(round).to.be.an.instanceof(Round);
       }); 
+
+    beforeEach(() => {
+        card1 = new Card(1, 'When was Colorado given its statehood?', [1876, 1976, 1865], 1876);
+        card2 = new Card(2, 'Who was Colorado\'s first Governor (as a state, not a territory)?', ['John Evans', 'John Long Routt', 'William Gilpin'], 'John Long Routt');
+        card3 = new Card(3, 'What is Colorado\'s highest peak?', ['Pike\'s Peak', 'Mount Evans', 'Mount Elbert'], 'Mount Elbert');
+
+        deck = new Deck([card1, card2, card3]);
+        round = new Round(deck);
+    });
     
     it('Should store a deck of cards', () => {
-        const card1 = new Card(1, 'When was Colorado given its statehood?', [1876, 1976, 1865], 1876);
-        const card2 = new Card(2, 'Who was Colorado\'s first Governor (as a state, not a territory)?', ['John Evans', 'John Long Routt', 'William Gilpin'], 'John Long Routt');
-        const card3 = new Card(3, 'What is Colorado\'s highest peak?', ['Pike\'s Peak', 'Mount Evans', 'Mount Elbert'], 'Mount Elbert');
-
-        const deck = new Deck([card1, card2, card3]);
-
-        const round = new Round(deck);
-
         expect(round.deck).to.equal(deck);
     });
+
+    it('Should have a turn property that begins at 0', () => {
+        expect(round.turns).to.equal(0);
+    })
+
 });
