e210bf8cf66558d5a2d736890cca04d654368c63
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-10 18:55:16 -0600

    Add test to assess that the Round stores a deck of cards

--- test/Round-test.js
+++ test/Round-test.js
@@ -16,5 +16,15 @@ describe ('Round', () => {
         expect(round).to.be.an.instanceof(Round);
       }); 
     
-    
+    it('Should store a deck of cards', () => {
+        const card1 = new Card(1, 'When was Colorado given its statehood?', [1876, 1976, 1865], 1876);
+        const card2 = new Card(2, 'Who was Colorado\'s first Governor (as a state, not a territory)?', ['John Evans', 'John Long Routt', 'William Gilpin'], 'John Long Routt');
+        const card3 = new Card(3, 'What is Colorado\'s highest peak?', ['Pike\'s Peak', 'Mount Evans', 'Mount Elbert'], 'Mount Elbert');
+
+        const deck = new Deck([card1, card2, card3]);
+
+        const round = new Round(deck);
+
+        expect(round.deck).to.equal(deck);
+    });
 });
