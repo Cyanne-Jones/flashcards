a82c00c5e7713f98ebf4c1c37ea7a62cdd82087a
Cyanne Jones <jonescyanne@yahoo.com>
2022-05-12 13:31:40 -0600

    Add beforeEach block to Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -4,57 +4,48 @@ const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', () => {
+    let card;
+    let turn;
+
+    beforeEach(() => {
+        card = new Card (1, 'Who was the drummer for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'Nick Mason');
+        turn = new Turn('Nick Mason', card);
+    });
 
     it('Should be a function', () => {
-        const turn = new Turn();
         expect(Turn).to.be.a('function');
     });
 
     it('should be an instance of Turn', () => {
-        const turn = new Turn();
         expect(turn).to.be.an.instanceof(Turn);
-      }); 
+    }); 
 
     it('Should store a user\'s guess', () => {
-        const turn = new Turn('This is my answer');
-        expect(turn.guess).to.equal('This is my answer');
+        expect(turn.guess).to.equal('Nick Mason');
     });
 
     it('Should store a card for the current card in play', () => {
-        const card = new Card (1, 'Who was the drummer for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'Nick Mason');
-        const turn = new Turn('Nick Mason', card);
-
         expect(turn.guess).to.equal('Nick Mason');
     });
 
     it('Should be able to return the user\'s guess', () => {
-        const card = new Card (2, 'Who was the guitarist for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'David Gilmour');
-        const turn = new Turn('David Gilmour', card);
-        
-        expect(turn.returnGuess()).to.equal('David Gilmour');
+        expect(turn.returnGuess()).to.equal('Nick Mason');
     });
     it('Should be able to return the current card in play', () => {
-        const card = new Card (3, 'Who was the keyboardist for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'Richard Wright');
-        const turn = new Turn('Richard Wright', card);
-
         expect(turn.returnCard()).to.equal(card);
     });
 
     it('Should be able to evaluate the user\'s guess', () => {
-        const card = new Card (3, 'Who was the bassist for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'Roger Waters');
-        const turn = new Turn('Richard Wright', card);
-        const turn2 = new Turn ('Roger Waters', card);
+        const turn2 = new Turn ('Roger Waters', card)
 
-        expect(turn.evaluateGuess()).to.equal(false);
-        expect(turn2.evaluateGuess()).to.equal(true);
+        expect(turn.evaluateGuess()).to.equal(true)
+        expect(turn2.evaluateGuess()).to.equal(false);
     });
 
     it('Should give feedback based on the user\'s answer', () => {
-        const card = new Card (3, 'Who was the bassist for Pink Floyd?', ['Roger Waters', 'David Gilmour', 'Richard Wright', 'Nick Mason'], 'Roger Waters');
-        const turn = new Turn('Richard Wright', card);
-        const turn2 = new Turn ('Roger Waters', card);
+        const turn2 = new Turn('Richard Wright', card)
 
-        expect(turn.giveFeedback()).to.equal('incorrect!');
-        expect(turn2.giveFeedback()).to.equal('correct!');
+        expect(turn.giveFeedback()).to.equal('correct!')
+        expect(turn2.giveFeedback()).to.equal('incorrect!');
     });
 });
